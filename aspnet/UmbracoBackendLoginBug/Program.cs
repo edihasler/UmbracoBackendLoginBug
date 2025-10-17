a951d38fc326d71a8987e5c29738715c1f53740f
edihasler <e.hasler@inetcons.it>
2025-10-17 15:59:12 +0200

    feat(dotnet): forwarded headers

--- aspnet/UmbracoBackendLoginBug/Program.cs
+++ aspnet/UmbracoBackendLoginBug/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedFor;
+    //options.KnownNetworks.Clear(); // Removes restrictions on proxy IP addresses
+    //options.KnownProxies.Clear(); // Allows Azure proxies to be trusted
+});
 
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
@@ -7,6 +16,20 @@ builder.CreateUmbracoBuilder()
     .Build();
 
 WebApplication app = builder.Build();
+
+app.UseForwardedHeaders();
+
+app.Use(async (context, next) =>
+{
+    var x = $"{context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}";
+    var headers = context.Request.Headers;
+
+    // Call next middleware
+    await next(context);
+
+    // After request processing
+    Console.WriteLine($"Response: {context.Response.StatusCode}");
+});
 
 await app.BootUmbracoAsync();
 
